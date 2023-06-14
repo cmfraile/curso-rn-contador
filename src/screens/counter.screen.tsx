081a190460dd8afdf914fde66bd32a060868b562
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-14 12:38:42 +0200

    59

--- src/screens/counter.screen.tsx
+++ src/screens/counter.screen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { View , Text , TouchableOpacity , ImageBackground } from "react-native";
 
@@ -6,10 +6,11 @@ const CustomFab = ({pom,setCallback}:{pom:boolean,setCallback:() => void}) => {
 
     const getSource = (pom)
     ? require('../assets/tora.png')
-    : require('../assets/ushio.png')
+    : require('../assets/ushio.png');
 
     return(
-        <TouchableOpacity onPress={setCallback} style={styles.customfab}>
+        //Puedes mandar estilos en array, y mandar mas de un estilo o generadores de estilo
+        <TouchableOpacity onPress={setCallback} style={[styles.customfab]}>
             <ImageBackground source={getSource} style={styles.backgroundImage}>
                 <Text style={styles.fabText}>{`${(pom) ? '+1' : '-1'}`}</Text>
             </ImageBackground>
@@ -18,13 +19,18 @@ const CustomFab = ({pom,setCallback}:{pom:boolean,setCallback:() => void}) => {
 
 }
 
+const hexCallback = () => `#${Math.random().toString(16).slice(2, 8).padStart(6, '0')}`
+
 const ContadorScreen = () => {
 
     const [ counter , setCounter ] = useState<number>(0);
+    const [ hex , setHex ] = useState<string>(hexCallback())
+
+    useEffect(() => setHex(hexCallback()),[counter])
 
     return(
         <View style={styles.container}>
-            <Text style={styles.text}> Contador </Text>
+            <Text style={{...styles.text,color:hex}}> Contador </Text>
             <Text style={styles.text}>{counter}</Text>
             <View style={styles.buttonGroup}>
                 <CustomFab pom={false} setCallback={() => setCounter(v => v-1)}/>
